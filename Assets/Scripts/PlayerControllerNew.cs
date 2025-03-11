39c7ce5f3161d673eb6029d1be972efc7a25a8ae
Shortieh <DarkShortyy@hotmail.de>
2025-03-11 18:46:22 +0100

    updated new player code

--- Assets/Scripts/PlayerControllerNew.cs
+++ Assets/Scripts/PlayerControllerNew.cs
@@ -1,28 +1,30 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Callbacks;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControllerNew : MonoBehaviour
 {           
     private Rigidbody2D rb;
 
-    private float faceDirectionX;
-    public float moveDirectionX;
-
-    private float minWalkSpeedX = .25f;
-    private float walkAccelarationX = .014f;
-    private float currentSpeedX;
-    private float maxWalkSpeedX = 5.5f;
-    private float releaseDecelerationX = .25f;
-    private float skidDecelerationX = .5f;
-    private float skidTurnaroundX = 3.5f;
-
-
+    //public LayerMask GroundLayers;
+    //private Transform groundCheck1, groundCheck2;
+    private float faceDirX; //direction player is facing
+    public float moveDirX; //direction of movement input
+    private float currentSpdX;
+    private float minSpdX = .25f; //minimum walk speed
+    private float maxSpdX = 10.5f;
+    private float accelX = .014f; 
+    private float decelX = .25f;
+    private float skidDecelX = .5f;
+    private float skidTurnX = 4f;
     private bool isChangingDirection;
 
-    //int xDir = 5;
-/*
+    //private bool isGrounded,isJumping,jumpHeld,jumpRelease;
+    //[SerializeField] private int Collectible = 0;
+    //[SerializeField] private Text CollectibleText;
+    //[SerializeField] private float jumpSpdY;
+    /*
     private Animator anim;
     private enum State {idle, run, jump, falling}; //Set a INT to idle, run and jump
     private State state = State.idle;
@@ -30,130 +32,79 @@ public class PlayerControllerNew : MonoBehaviour
     [SerializeField] private LayerMask Ground; */
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    void Start(){ 
         rb = GetComponent<Rigidbody2D>();
+        //groundCheck1 = transform.Find("groundCheck1");
+        //groundCheck2 = transform.Find("groundCheck2");
+        //normalGravity = rb.gravityScale;
         //anim = GetComponent<Animator>();
         //coll = GetComponent<Collider2D>();
     }
 
+   /* void setJumpParams(){
+        jumpSpdY = 15f;
+    }*/
     // Update is called once per frame
-    void Update()
-    {
-        float faceDirectionX = Input.GetAxisRaw("Horizontal"); 
+    void Update(){
+        Debug.Log("Current Speed:"+currentSpdX);
+        float faceDirX = Input.GetAxisRaw("Horizontal");
+    //    isGrounded = Physics2D.OverlapPoint(groundCheck1.position,GroundLayers) || Physics2D.OverlapPoint(groundCheck2.position,GroundLayers);
 
-        if (faceDirectionX != 0){ //if player is moving
-            if(currentSpeedX == 0){ //if player is moving but they stood still before
-
-                currentSpeedX = minWalkSpeedX;   
-
-            }else if(currentSpeedX < maxWalkSpeedX){ //player is moving but less than their max walk speed-up
-
-                currentSpeedX = IncreaseWithinBound(currentSpeedX, walkAccelarationX, maxWalkSpeedX); //accelerate, then keep at max speed
-
+        if (faceDirX != 0){ //if player is moving
+            if(currentSpdX == 0){ //if player starts movement from a halt
+                currentSpdX = minSpdX;
+            }else if(currentSpdX < maxSpdX){ //player is moving but less than their max walk speed-up
+                currentSpdX = increaseVelocity(currentSpdX,accelX,maxSpdX); //keep at max speed
             }
-        }else if(currentSpeedX > 0){
-
-            currentSpeedX = DecreaseWithinBound(currentSpeedX, releaseDecelerationX, 0);
-
+        }else if(currentSpdX > 0){
+            currentSpdX = decreaseVelocity(currentSpdX,decelX);
         }
-
-        isChangingDirection = currentSpeedX > 0 && faceDirectionX * moveDirectionX < 0; //if player is moving + in negative direction
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
-       if (isChangingDirection)
-        {
-            if (currentSpeedX > skidTurnaroundX)
-            {
-                moveDirectionX = -moveDirectionX;  // instead of -faceDirectionX (avoids 0-problem)
-                currentSpeedX = DecreaseWithinBound(currentSpeedX, skidDecelerationX, 0);
-                Debug.Log("Richtungswechsel: Geschwindigkeit wird abgebremst. currentSpeedX: " + currentSpeedX);
-            }else
-            {
-                moveDirectionX = faceDirectionX;
-                currentSpeedX = minWalkSpeedX;  // 🚀 instant reset to min-speed after turnaround
-                Debug.Log("Richtungswechsel: Geschwindigkeit wird auf minWalkSpeedX gesetzt. currentSpeedX: " + currentSpeedX);
-            }
-        }
-        else
-        {
-            if (faceDirectionX != 0)
-            {
-                moveDirectionX = faceDirectionX;
-            }
-
-            // 🚀 instant turnaroundif we stand still an change direction
-            if (currentSpeedX < minWalkSpeedX) 
-            {
-                moveDirectionX = minWalkSpeedX;  
-                Debug.Log("Aktualisierung: Geschwindigkeit auf minWalkSpeedX gesetzt. currentSpeedX: " + currentSpeedX);
-            }
-        }
-
-         rb.linearVelocity = new Vector2(moveDirectionX * currentSpeedX, rb.linearVelocity.y);
-
-        if(faceDirectionX > 0){
-
-            transform.localScale = new Vector2(1, 1); //sprite facing right
-        }
-        else if(faceDirectionX < 0){
-            transform.localScale = new Vector2(-1, 1); //sprite facing left
-              
-        }
-
         
+    
+        isChangingDirection = currentSpdX > 0 && faceDirX * moveDirX < 0; //if player is moving in opposite direction
 
+        if(isChangingDirection){
+            if(currentSpdX > skidTurnX){   
+                moveDirX = -faceDirX;
+                currentSpdX = decreaseVelocity(currentSpdX,skidDecelX,0);
+            }else{
+                moveDirX = faceDirX;
+            }
+        }
+
+        rb.linearVelocity = new Vector2(moveDirX*currentSpdX,rb.linearVelocity.y);
+
+        if(faceDirX > 0){
+            transform.localScale = new Vector2(1,1); //sprite facing right
+        }
+        else if(faceDirX < 0){
+            transform.localScale = new Vector2(-1,1); //sprite facing left           
+        }
     }
 
-    float IncreaseWithinBound(float val, float delta, float maxVal){
+    /*private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.tag == "Collectible"){
+            Destroy(collision.gameObject);
+            Collectible += 1;
+            CollectibleText.text = Collectible.ToString();
+        }    
+    }*/
 
+    float increaseVelocity(float val, float delta, float maxVal){
         val += delta; //speed increases
 
         if(val > maxVal){ //speed limit
-
             val = maxVal;
-
         }
-
         return val;
     }
 
-    float DecreaseWithinBound(float val, float delta, float minVal = 0){
+    float decreaseVelocity(float val, float delta, float minVal = 0){
+        val -= delta; //speed decreases
 
-        val -= delta * 1.5f; //decrease speed more heavily to make it fluid
-        if(val < minVal){ //speed limit
-
+        if(val < minVal){ //minimum speed
             val = minVal;
-
         }
-
         return val;
-
     }
-
-    /*private void VelocityState(){
-
-            if(state == State.jump){
-
-                if(rb.linearVelocity.y < .1f){
-                    state = State.falling;
-                }
-            }   
-                else if(state == State.falling){
-                    if(coll.IsTouchingLayers(Ground)){
-                        state = State.idle;
-                    }
-                }
-            
-            else if(Mathf.Abs(rb.linearVelocity.x) > 2f){
-
-                state = State.run;
-            }
-            else{
-
-                state = State.idle;
-            }
-
-    }*/
-
 }
